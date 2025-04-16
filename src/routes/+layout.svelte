cc68e3ce3662f173fcad077aa83bba7382267a3b
cubedhuang <99danielh@gmail.com>
2025-04-16 00:04:04 -0400

    remove max width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	let { children } = $props();
 </script>
 
-<div class="mx-auto mb-20 mt-8 max-w-screen-xl px-8">
+<div class="mb-20 mt-8 px-8">
 	<nav class="mb-8 flex gap-8 text-gray-500">
 		<a href="/" class="hover:text-black">home</a>
 		<a href="/analyze" class="hover:text-black">counter</a>
